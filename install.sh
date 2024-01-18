832e113910415112233ef6b925651edbafbaf167
Lucio Rossi <lucio.rossi75@gmail.com>
2024-01-18 13:37:28 +0100

    mod: flash firmware utilities update. flash_firmware.sh is separated from install.sh and works only on files named firmware.bin

--- install.sh
+++ install.sh
@@ -20,22 +20,4 @@ $python_command -m mpremote cp constants.py :constants.py
 $python_command -m mpremote cp pinout_definitions.py :pinout_definitions.py
 $python_command -m mpremote cp uart.py :uart.py
 
-echo "Do you need flash firmware utilities? (Y/n)"
-read -r needs_flash
-needs_flash="${needs_flash:-y}"
-
-if [ "$needs_flash" == "y" ]; then
-    $python_command -m mpremote rm :firmware_updater.py
-    $python_command -m mpremote rm :stm32_flash.py
-
-    $python_command -m mpremote cp ./utilities/firmware_updater.py :firmware_updater.py
-    $python_command -m mpremote cp stm32_flash.py :stm32_flash.py
-
-    echo "Type the name of the firmware you want to upload. (Must be in the main folder. Press Enter to skip)"
-    read -r firmware_file
-    if [ "$firmware_file" != "" ]; then
-        $python_command -m mpremote cp $firmware_file :$firmware_file
-    fi
-fi
-
 $python_command -m mpremote reset
---
+++ utilities/flash_firmware.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if command -v python3 &>/dev/null; then
+    python_command="python3"
+else
+    # If python3 is not available, use python
+    python_command="python"
+fi
+
+# Uncomment the following line on windows machines
+# python_command="python"
+
+echo "Installing flash firmware utilities..."
+
+$python_command -m mpremote rm :firmware_updater.py
+$python_command -m mpremote rm :stm32_flash.py
+
+$python_command -m mpremote cp ./firmware_updater.py :firmware_updater.py
+$python_command -m mpremote cp ./stm32_flash.py :stm32_flash.py
+
+echo "Uploading your firmware file. There must be a file named firmware.bin in the same folder as this sh script. Press Enter to skip"
+
+$python_command -m mpremote rm :firmware.bin
+$python_command -m mpremote cp ./firmware.bin :firmware.bin
+
+echo "Do want to flash the firmware right now? (y/N)"
+read do_flash
+
+if [ "$do_flash" == "y" || "$do_flash" == "Y" ]; then
+  $python_command -m mpremote run firmware_updater.py
+else
+  echo "Firmware was not flashed on the remote device."
+fi
+
+$python_command -m mpremote reset
