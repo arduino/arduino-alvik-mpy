c17c9a30c1f3ec9a8726c968d13e746f51ef1670
Lucio Rossi <lucio.rossi75@gmail.com>
2024-02-23 14:31:36 +0100

    fix: alvikdev-58 FW updater stuck if robot is off

--- utilities/firmware_updater.py
+++ utilities/firmware_updater.py
@@ -1,9 +1,14 @@
 from sys import exit
 from stm32_flash import *
 
+if CHECK_STM32.value() is not 1:
+    print("Turn on your Alvik to continue...")
+    while CHECK_STM32.value() is not 1:
+        sleep_ms(500)
+
 ans = STM32_startCommunication()
 if ans == STM32_NACK:
-    print("Cannot etablish connection with STM32")
+    print("Cannot establish connection with STM32")
     exit(-1)
 
 print('\nSTM32 FOUND')
