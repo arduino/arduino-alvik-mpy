c019b05802b6a20e4481b52331b0c7088cc438a2
Lucio Rossi <lucio.rossi75@gmail.com>
2024-11-08 16:26:26 +0100

    fix: reimport all the modules before flashing a new FW

--- examples/flash_firmware.py
+++ examples/flash_firmware.py
@@ -1,4 +1,19 @@
-# from machine import reset
+import sys
+
+
+def reload_modules():
+    to_be_reloaded = []
+
+    for m in sys.modules:
+        to_be_reloaded.append(m)
+        del sys.modules[m]
+
+    for m in to_be_reloaded:
+        print(f"Reloading {m}")
+        exec(f'import {m}')
+
+
+reload_modules()
 from arduino_alvik import update_firmware
 
 # this is a patch to fix possible running threads on Alvik
@@ -6,5 +21,4 @@ from arduino_alvik import ArduinoAlvik
 alvik = ArduinoAlvik()
 alvik.stop()
 
-update_firmware('./firmware.bin')
-# reset()
+update_firmware('/firmware.bin')
