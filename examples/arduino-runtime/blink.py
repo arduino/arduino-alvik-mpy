c9376b94dd7bfe65f06cefb4f80e22b88caa9e70
Lucio Rossi <lucio.rossi75@gmail.com>
2024-11-20 17:47:27 +0100

    feat: runtime examples blink

---
+++ examples/arduino-runtime/blink.py
@@ -0,0 +1,24 @@
+from time import sleep_ms
+
+from arduino import start
+from arduino_alvik import ArduinoAlvik
+
+
+alvik = ArduinoAlvik()
+
+
+def setup():
+    alvik.begin()
+
+
+def loop():
+    print('loopy loop')
+    alvik.left_led.set_color(0, 0, 1)
+    alvik.right_led.set_color(0, 0, 1)
+    sleep_ms(500)
+    alvik.left_led.set_color(1, 0, 0)
+    alvik.right_led.set_color(1, 0, 0)
+    sleep_ms(500)
+
+
+start(setup, loop)
